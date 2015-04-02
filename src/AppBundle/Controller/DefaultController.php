3228acded36b82802ed537429286bea615355afb
Darius S <pdst@bite.lt>
2015-04-02 14:19:59 +0300

    Acme bundle removed, added role for root path

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,14 +4,18 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/app/example", name="homepage")
+     * @Security("has_role('ROLE_USER')")
+     * @Route("/", name="homepage")
      */
     public function indexAction()
     {
+        // Logout user
+        // $this->container->get('security.context')->setToken(null);
         return $this->render('default/index.html.twig');
     }
 }
