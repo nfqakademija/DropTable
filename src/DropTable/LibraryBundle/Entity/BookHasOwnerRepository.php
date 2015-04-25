3030757f0b3640775194afc54ea44bb34b4031ca
Darius S <darius.staniulis@gmail.com>
2015-04-25 16:59:05 +0300

    Implemented function to get Available owners of the book and code styling fixes

--- src/DropTable/LibraryBundle/Entity/BookHasOwnerRepository.php
+++ src/DropTable/LibraryBundle/Entity/BookHasOwnerRepository.php
@@ -4,19 +4,34 @@ namespace DropTable\LibraryBundle\Entity;
 
 use Doctrine\ORM\EntityRepository;
 
+/**
+ * Class BookHasOwnerRepository.
+ *
+ * @package DropTable\LibraryBundle\Entity
+ */
 class BookHasOwnerRepository extends EntityRepository
 {
+    /**
+     * Function findAllAvailableOwners.
+     *
+     * @param Book $book
+     *
+     * @return array
+     */
     public function findAllAvailableOwners(Book $book)
     {
-        $book_id = $book->getId();
-
-
         $qb = $this->getEntityManager()->createQueryBuilder()
             ->select('owner')
             ->from('DropTableLibraryBundle:BookHasOwner', 'owner')
-            ->leftJoin('DropTableLibraryBundle:UserHasReservation', 'reservation', 'WITH', 'reservation.bookHasOwner = owner')
-//            ->where('owner.id = :ids')
-//            ->setParameter('id', '')
+            ->leftJoin(
+                'DropTableLibraryBundle:UserHasReservation',
+                'reservation',
+                'WITH',
+                'reservation.bookHasOwner = owner'
+            )
+            ->where('reservation.bookHasOwner IS NULL')
+            ->andWhere('owner.book = :book_id')
+            ->setParameter('book_id', $book)
             ->getQuery()
             ->getResult();
 
