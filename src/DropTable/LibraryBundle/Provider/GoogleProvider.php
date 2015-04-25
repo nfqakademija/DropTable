9cc9a68ca3601d9e4810c0b533822063083e33c3
Darius S <darius.staniulis@gmail.com>
2015-04-25 17:04:05 +0300

    ONGR strict standard fixes

--- src/DropTable/LibraryBundle/Provider/GoogleProvider.php
+++ src/DropTable/LibraryBundle/Provider/GoogleProvider.php
@@ -5,17 +5,38 @@ namespace DropTable\LibraryBundle\Provider;
 use Buzz\Browser;
 use DropTable\LibraryBundle\Parser\GoogleProviderParser;
 
+/**
+ * Class GoogleProvider.
+ *
+ * @package DropTable\LibraryBundle\Provider
+ */
 class GoogleProvider implements BookProviderInterface
 {
+    /**
+     * @var Browser
+     */
     protected $buzz;
+
+    /**
+     * @var GoogleProviderParser
+     */
     protected $parser;
 
+    /**
+     * @param Browser              $buzz
+     * @param GoogleProviderParser $parser
+     */
     public function __construct(Browser $buzz, GoogleProviderParser $parser)
     {
         $this->buzz = $buzz;
         $this->parser = $parser;
     }
 
+    /**
+     * @param string $isbn
+     *
+     * @return array
+     */
     public function getBook($isbn)
     {
         $url = "https://www.googleapis.com/books/v1/volumes/?q=isbn:{$isbn}";
