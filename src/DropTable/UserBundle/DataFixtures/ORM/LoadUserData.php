e389cf1a6c3cfbb8e7459d3dc4b1992db1eced45
Darius S <darius.staniulis@gmail.com>
2015-05-01 15:17:33 +0300

    Updated user fixtures to FOSUserBundle and modified book provider to return book entity if no book found

--- src/DropTable/UserBundle/DataFixtures/ORM/LoadUserData.php
+++ src/DropTable/UserBundle/DataFixtures/ORM/LoadUserData.php
@@ -5,36 +5,77 @@ namespace DropTable\UserBundle\DataFixtures\ORM;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
-use DropTable\UserBundle\Entity\User;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Class LoadUserData.
  *
  * @package DropTable\UserBundle\DataFixtures\ORM
  */
-class LoadUserData extends AbstractFixture implements OrderedFixtureInterface
+class LoadUserData extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
 {
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function load(ObjectManager $manager)
     {
-        $user1 = new User();
-        $user1->setFirstName('Vardenis');
-        $user1->setLastName('Pavardenis');
-        $user1->setUsername('user1@');
+        $test_password = 'test';
 
-        $user2 = new User();
-        $user2->setFirstName('Vardenis1');
-        $user2->setLastName('Pavardenis1');
-        $user2->setUsername('user2@');
+        $factory = $this->container->get('security.encoder_factory');
 
-        $this->addReference('user1', $user1);
-        $this->addReference('user2', $user2);
 
-        $manager->persist($user1);
-        $manager->persist($user2);
-        $manager->flush();
+        /** @var $manager \FOS\UserBundle\Doctrine\UserManager */
+        $manager = $this->container->get('fos_user.user_manager');
+
+        /** @var $user \DropTable\UserBundle\Entity\User */
+        $user = $manager->createUser();
+
+        $user->setUsername('Admin');
+        $user->setEmail('admin@dorptable.library');
+        $user->setFirstname('Admin');
+        $user->setLastname('Admin');
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->setEnabled(true);
+        $encoder = $factory->getEncoder($user);
+        $password = $encoder->encodePassword($test_password, $user->getSalt());
+        $user->setPassword($password);
+
+        $manager->updateUser($user);
+
+        $this->addReference('user1', $user);
+
+        unset($user);
+
+        /** @var $user \DropTable\UserBundle\Entity\User */
+        $user = $manager->createUser();
+
+        $user->setUsername('User');
+        $user->setEmail('user@droptable.library');
+        $user->setFirstname('User');
+        $user->setLastname('User');
+        $user->setRoles(['ROLE_USER']);
+        $user->setEnabled(true);
+        $encoder = $factory->getEncoder($user);
+        $password = $encoder->encodePassword($test_password, $user->getSalt());
+        $user->setPassword($password);
+
+        $manager->updateUser($user);
+
+        $this->addReference('user2', $user);
     }
 
     /**
