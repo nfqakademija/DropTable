7cf1f608b76cd50b57bf14d3bb2060b176ce2973
justaserr <altergt>
2015-05-07 22:38:15 +0300

    Started creating template files. Login and register almost done, somewhy imported (as opposed to inline) <script> does not work, so Facebook popup dialog box does not work. Commented out 127.0.0.1 requirement in app_dev.php for deployment into server.

--- src/DropTable/UserBundle/Controller/DefaultController.php
+++ src/DropTable/UserBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace DropTable\UserBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -12,7 +13,7 @@ class DefaultController extends Controller
 
     public function __construct()
     {
-        $this->em = $this->get('request');
+//        $this->em = $this->get('request');
         // parent::__construct();
     }
 
