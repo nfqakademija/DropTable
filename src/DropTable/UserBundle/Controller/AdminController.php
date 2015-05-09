92987ad767d8f1ec73becd51feb949277a46c17f
justaserr <altergt>
2015-05-09 11:53:50 +0300

    Disable writing of password request time to remove the constraint of password requesting once per 24 hours.

--- src/DropTable/UserBundle/Controller/AdminController.php
+++ src/DropTable/UserBundle/Controller/AdminController.php
@@ -4,7 +4,6 @@ namespace DropTable\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Class AdminController
